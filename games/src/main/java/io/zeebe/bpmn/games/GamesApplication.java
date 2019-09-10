6ab835db20a4dce3bebe0882952bace2b1160d98
Christopher Zell <zelldon91@googlemail.com>
2019-09-10 14:55:50 +0200

    adjust timeouts

--- games/src/main/java/io/zeebe/bpmn/games/GamesApplication.java
+++ games/src/main/java/io/zeebe/bpmn/games/GamesApplication.java
@@ -125,7 +125,8 @@ public class GamesApplication {
           .newWorker()
           .jobType(jobTypeHandler.getKey())
           .handler(jobTypeHandler.getValue())
-          .timeout(Duration.ofHours(1))
+          .timeout(Duration.ofMinutes(10))
+          .requestTimeout(Duration.ofHours(1))
           .open();
     }
   }
